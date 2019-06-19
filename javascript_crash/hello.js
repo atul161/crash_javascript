bf22b9721f1411a3d0e9000a8810d5463fc5e85c
atul anand <37778673+atul161@users.noreply.github.com>
2019-06-19 09:03:12 +0530

    Add files via upload

---
+++ javascript_crash/hello.js
@@ -0,0 +1,1 @@
+console.log("Hello Everyone This is Atul Anand")
---
+++ javascript_crash/let_varaible.js
@@ -0,0 +1,14 @@
+console.log('Welcom to let varaible');
+
+let a;
+a = 2;
+console.log(a)
+
+let b, c, d;
+b = 25;
+c = "Atul";
+d = 56.9;
+console.log(b + c + d)
+
+let f = 26
+console.log(f)
